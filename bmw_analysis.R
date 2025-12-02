6ef1539d0cfd7522f9ab54022284bdfbb0ceeb88
Ramesh <mr25ack@herts.ac.uk>
2025-12-02 17:28:51 +0000

    added code to R file

--- bmw_analysis.R
+++ bmw_analysis.R
@@ -8,15 +8,32 @@
 library(readr)
 library(tidyverse)
 
-
 # 2. Load the BMW dataset
 bmw <- read_csv("BMW sales data (2010-2024) (1).csv")
-
-
-
-
 
 #data cleaning
 #check any NA in the data set and display the count 
 nrow(is.na(bmw_sales))
 
+
+
+
+
+
+
+
+
+
+
+
+
+#A scatterplot to include the linear trendline
+plot(bmw_dataClean$Mileage_KM, bmw_dataClean$Price_USD,
+     xlab = "Mileage (KM)",
+     ylab = "Price (USD)",
+     main = "Mileage vs Price of BMW Used Cars",
+)
+abline(lm(bmw_dataClean$Mileage_KM ~ bmw_dataClean$Price_USD), col = "red")
+
+#Paired t -test
+t.test(bmw_dataClean$Mileage_KM, bmw_dataClean$Price_USD, paired = TRUE)
