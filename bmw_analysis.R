1a93afb962c0daf904c5c2f5489cd3e11f593d47
Arone Sahayaraj <as25afv@herts.ac.uk>
2025-12-02 04:44:01 -1200

    Data cleaning: added code for data cleaning

--- bmw_analysis.R
+++ bmw_analysis.R
@@ -6,8 +6,17 @@
 
 # 1. Load libraries
 library(readr)
+library(tidyverse)
 
 
 # 2. Load the BMW dataset
 bmw <- read_csv("BMW sales data (2010-2024) (1).csv")
+
+
+
+
+
+#data cleaning
+#check any NA in the data set and display the count 
+nrow(is.na(bmw_sales))
 
