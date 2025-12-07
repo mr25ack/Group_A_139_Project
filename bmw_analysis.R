37250cb867093c46ce118960aee5d9748dfc6711
Farhana Akter Bristy <fb25aam@herts.ac.uk>
2025-12-07 20:30:39 +0000

    bmw_analysis.R

--- bmw_analysis.R
+++ bmw_analysis.R
@@ -11,6 +11,13 @@ library(tidyverse)
 # 2. Load the BMW dataset
 BMW_sales <- read_csv("BMW sales data (2010-2024) (1).csv")
 
+#distribution of data
+# Price_USD, Sales_Volume, Mileage_KM, Engine_Size_L are continuous variables of our dataset.
+hist(BMW_sales_data_2010_2024_1_$Price_USD, main = "Histogram of car prices")
+hist(BMW_sales_data_2010_2024_1_$Engine_Size_L, main = "Histogram of engine sizes")
+hist(BMW_sales_data_2010_2024_1_$Sales_Volume, main = "Histogram of sales volume")
+hist(BMW_sales_data_2010_2024_1_$Mileage_KM, main = "Histogram of mileage in KM")
+
 #data cleaning
 #check any NA in the data set and display the count 
 nrow(is.na(bmw_sales))
@@ -39,4 +46,5 @@ plot(BMW_dataClean$Mileage_KM, BMW_dataClean$Price_USD,
 abline(lm(BMW_dataClean$Mileage_KM ~ BMW_dataClean$Price_USD), col = "red")
 
 #Paired t -test
+
 t.test(BMW_dataClean$Mileage_KM, BMW_dataClean$Price_USD, paired = TRUE)
