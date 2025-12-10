51a3ee1f651d44da3edc8afae2831fba82a4d9a0
Meena Ramesh <mr25ack@herts.ac.uk>
2025-12-10 19:17:59 +0000

    The R code for the stacked bar chart visualization was added to the R script.

--- bmw_analysis.R
+++ bmw_analysis.R
@@ -4,19 +4,20 @@
 # "Is there a difference in proportions of sales of BMW car models 
 #  between Asia and Africa from 2010 to 2024?"
 
-# 1. Load libraries
+# Load libraries
 library(readr)
 library(tidyverse)
+library(tibble)
 
-# 2. Load the BMW dataset
-BMW_sales <- read_csv("BMW sales data (2010-2024) (1).csv")
+#Load the BMW dataset
+BMW_sales_data_2010_2024_1_ <- read_csv("BMW sales data (2010-2024) (1).csv")
 
-# 3. understand dataset
+# understand dataset
 View(BMW_sales_data_2010_2024_1_)
 print(colnames(BMW_sales_data_2010_2024_1_))
 nrow(BMW_sales_data_2010_2024_1_)
 print(head(BMW_sales_data_2010_2024_1_))
-library(tibble)
+
 print(glimpse(BMW_sales_data_2010_2024_1_))
 summary(BMW_sales_data_2010_2024_1_$Engine_Size_L)
 summary(BMW_sales_data_2010_2024_1_$Mileage_KM)
@@ -24,11 +25,9 @@ summary(BMW_sales_data_2010_2024_1_$Price_USD)
 summary(BMW_sales_data_2010_2024_1_$Sales_Volume)
 
 
-
-
 #data cleaning
 #check any NA in the data set and display the count 
-nrow(is.na(bmw_sales))
+nrow(is.na(BMW_sales_data_2010_2024_1_))
 
 #distribution of data
 # Price_USD, Sales_Volume, Mileage_KM, Engine_Size_L are continuous variables of our dataset.
@@ -45,6 +44,22 @@ chisq.test(pt)
 #Contingency table
 print(pt)
 
+# Convert to proportions per model (column-wise)
+percentages <- prop.table(pt, margin = 2) * 100
+
+#opens the file for writing png file
+png("Stacked_Bar_Chart.png")
+# Plot stacked bar chart of proportions
+barplot(percentages,
+        col = c("pink", "blue"),           # Asia = pink, Africa = blue
+        xlab = "Model",
+        ylab = "Percentage",
+        main = "Stacked Bar Of BMW car models: Asia vs Africa",
+        ylim = c(0, 100),
+        legend.text = c("Asia", "Africa"),
+        args.legend = list(x = "topright"), las = 2, cex.names = 0.8, mgp = c(4, 1, 0))  
+# closes the png file 
+dev.off()
 
 
 
